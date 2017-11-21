e70795e5107acb35caecf21b41c3bc7ba68312c4
BubblyFace <751835145@qq.com>
2017-11-21 20:38:53 +0800

    finish with 2-layer-conv

--- index.py
+++ index.py
@@ -1,130 +1,81 @@
-from __future__ import print_function
+import tensorflow as tf
+import input_data
+import numpy as np 
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-import random
-import numpy as np
-from utils.data_utils import load_CIFAR10
-import matplotlib.pyplot as plt
+# x ----> input [784]  y_ ----> fact result [10]
+x = tf.placeholder("float", shape=[None, 784])
+y_ = tf.placeholder("float", shape=[None, 10])
 
-from src.classifier.KNN import KNearestNeighbor
+# W
+def weight_variable(shape):
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
 
+# b
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
 
-plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
-plt.rcParams['image.interpolation'] = 'nearest'
-plt.rcParams['image.cmap'] = 'gray'
-
-
-cifar10_dir = 'data/cifar-10-batches-py'
-X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
-print('Training data shape: ', X_train.shape)
-print('Training labels shape: ', y_train.shape)
-print('Test data shape: ', X_test.shape)
-print('Test labels shape: ', y_test.shape)
-
-
-# Visualize some examples from the dataset.
-# We show a few examples of training images from each class.
-classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-num_classes = len(classes)
-samples_per_class = 7
-for y, cls in enumerate(classes):
-    idxs = np.flatnonzero(y_train == y)
-    idxs = np.random.choice(idxs, samples_per_class, replace=False)
-    for i, idx in enumerate(idxs):
-        plt_idx = i * num_classes + y + 1
-        plt.subplot(samples_per_class, num_classes, plt_idx)
-        plt.imshow(X_train[idx].astype('uint8'))
-        plt.axis('off')
-        if i == 0:
-            plt.title(cls)
+# 卷积层的定义
+# tf.nn.conv2d(input, filter, strides, padding, use_cudnn_on_gpu=None, 
+# data_format=None, name=None)
+def conv2d(x, W):
+  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 
-# Subsample the data for more efficient code execution in this exercise
-num_training = 5000
-mask = list(range(num_training))
-X_train = X_train[mask]
-y_train = y_train[mask]
+# 最大池化层
+# 2 x 2
+def max_pool_2x2(x):
+  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                        strides=[1, 2, 2, 1], padding='SAME')
 
-num_test = 500
-mask = list(range(num_test))
-X_test = X_test[mask]
-y_test = y_test[mask]
+# first conv_layer
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
 
+# translate x into 4d v whitch[?, weight, height, rbg] = [-1, 28, 28, 1]
+x_image = tf.reshape(x, [-1,28,28,1])
 
-# Create a kNN classifier instance. 
-# Remember that training a kNN classifier is a noop: 
-# the Classifier simply remembers the data and does no further processing 
-classifier = KNearestNeighbor()
-classifier.train(X_train, y_train)
+# use relu as the function  and use max pooling at the end
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
 
-dists = classifier.compute_distances_two_loops(X_test)
-print(dists.shape)
+# the second layer
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
 
-plt.imshow(dists, interpolation='none')
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
 
-# Now implement the function predict_labels and run the code below:
-# We use k = 1 (which is Nearest Neighbor).
-y_test_pred = classifier.predict_labels(dists, k=1)
+# full-connect layer:Now we got a image witch with a size into 7x7 
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
 
-# Compute and print the fraction of correctly predicted examples
-num_correct = np.sum(y_test_pred == y_test)
-accuracy = float(num_correct) / num_test
-print('Got %d / %d correct => accuracy: %f' % (num_correct, num_test, accuracy))
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-y_test_pred = classifier.predict_labels(dists, k=5)
-num_correct = np.sum(y_test_pred == y_test)
-accuracy = float(num_correct) / num_test
-print('Got %d / %d correct => accuracy: %f' % (num_correct, num_test, accuracy))
+keep_prob = tf.placeholder("float")
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
 
-# Now lets speed up distance matrix computation by using partial vectorization
-# with one loop. Implement the function compute_distances_one_loop and run the
-# code below:
-dists_one = classifier.compute_distances_one_loop(X_test)
+y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
-# To ensure that our vectorized implementation is correct, we make sure that it
-# agrees with the naive implementation. There are many ways to decide whether
-# two matrices are similar; one of the simplest is the Frobenius norm. In case
-# you haven't seen it before, the Frobenius norm of two matrices is the square
-# root of the squared sum of differences of all elements; in other words, reshape
-# the matrices into vectors and compute the Euclidean distance between them.
-difference = np.linalg.norm(dists - dists_one, ord='fro')
-print('Difference was: %f' % (difference, ))
-if difference < 0.001:
-    print('Good! The distance matrices are the same')
-else:
-    print('Uh-oh! The distance matrices are different')
+sess = tf.InteractiveSession()
+cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+sess.run(tf.initialize_all_variables())
+for i in range(20000):
+  batch = mnist.train.next_batch(50)
+  if i%100 == 0:
+    train_accuracy = accuracy.eval(feed_dict={
+        x:batch[0], y_: batch[1], keep_prob: 1.0})
+    print("step %d, training accuracy %g"%(i, train_accuracy))
+  train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-
-# Now implement the fully vectorized version inside compute_distances_no_loops
-# and run the code
-dists_two = classifier.compute_distances_no_loops(X_test)
-
-# check that the distance matrix agrees with the one we computed before:
-difference = np.linalg.norm(dists - dists_two, ord='fro')
-print('Difference was: %f' % (difference, ))
-if difference < 0.001:
-    print('Good! The distance matrices are the same')
-else:
-    print('Uh-oh! The distance matrices are different')
-
-# Let's compare how fast the implementations are
-def time_function(f, *args):
-    """
-    Call a function f with args and return the time (in seconds) that it took to execute.
-    """
-    import time
-    tic = time.time()
-    f(*args)
-    toc = time.time()
-    return toc - tic
-
-two_loop_time = time_function(classifier.compute_distances_two_loops, X_test)
-print('Two loop version took %f seconds' % two_loop_time)
-
-one_loop_time = time_function(classifier.compute_distances_one_loop, X_test)
-print('One loop version took %f seconds' % one_loop_time)
-
-no_loop_time = time_function(classifier.compute_distances_no_loops, X_test)
-print('No loop version took %f seconds' % no_loop_time)
-
-# you should see significantly faster performance with the fully vectorized implementation
+print("test accuracy %g"%accuracy.eval(feed_dict={
+    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
